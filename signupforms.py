c911372e1fafbd705a0787b1685ba3adf288aa67
KishnaPrasad <krishnaprasadpambavasan@gmail.com>
2020-07-25 10:49:38 +0530

    First commit for heroku

--- signupforms.py
+++ signupforms.py
@@ -16,10 +16,12 @@ class SignupForm(FlaskForm):
 	"""Sign up for a user account."""
 	name = StringField('Name',[DataRequired(), Length(min=4, max=26)])
 	email = StringField('Email',[InputRequired()])
-	#, [
+	password = PasswordField('Password',[InputRequired(),EqualTo('confirmpassword',message='Passwords must match')])
+	confirmpassword = PasswordField('Repeat Password',[InputRequired(), EqualTo(password, message='Passwords must match')])
+	submit = SubmitField('Submit')
+
+
+ 	#, [
      #    Email(message="Not a valid email address."),
       #   DataRequired()])
  	# email = EmailField('Email address',[validators.DataRequired(),validators.Email()])
- 	password = PasswordField('Password',[InputRequired(), EqualTo('confirmpassword', message='Passwords must match')])
- 	confirmpassword = PasswordField('Repeat Password',[InputRequired(), EqualTo(password, message='Passwords must match')])
- 	submit = SubmitField('Submit')
